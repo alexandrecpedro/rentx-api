70db5f64e2808cce2eebb9ad64e23024b690894a
Alexandre Pedro <alexandrecpedro@gmail.com>
2023-08-10 15:29:56 -0300

    RentX API

--- dist/shared/infra/http/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _typeorm = require("@shared/infra/typeorm");
-var _app = require("./app");
-(0, _typeorm.createConnection)();
-const port = Number(process.env.PORT) || 3333;
-_app.app.listen(port, () => console.log(`Server is running at port ${port}`));
